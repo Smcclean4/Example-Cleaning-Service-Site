28b0d8ca1555320a1c085fc0a20d1664c87529b5
Sidney McClean <sidney.mcclean@outlook.com>
2019-04-09 18:21:20 -0700

    added simple css animation to page

--- project/js/script.js
+++ project/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
 });
+
+window.onload = function showHome() {
+  const t1 = document.getElementById('bg').querySelectorAll('.image-container1', '.image-container2', '#m-statement', '#home-button');
+  setInterval(function () {
+
+  }, 500);
+
+}
+
+showHome();
