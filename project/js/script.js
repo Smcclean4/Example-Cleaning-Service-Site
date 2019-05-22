00bc58683b6dc4931a99b427dfc83950269e7b66
Sidney McClean <sidney.mcclean@outlook.com>
2019-05-21 22:49:10 -0700

    added favicon and formspree integration

--- project/js/script.js
+++ project/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 
+$('#bg').css('display', 'none').fadeOut(100).fadeIn(5000);
+$('#home-bg').css('display', 'none').fadeOut(100).fadeIn(4000);
+$('#aboutus-bg').css('display', 'none').fadeOut(100).fadeIn(4000);
+$('#contactus-bg').css('display', 'none').fadeIn(4000);
+$('#employment-bg').css('display', 'none').fadeIn(4000);
+$('#appointment-bg').css('display', 'none').fadeIn(4000);
+$('#review-bg').css('display', 'none').fadeIn(5000);
+
 });
-// still working.,
+
+
+
+
+// still working on it.
